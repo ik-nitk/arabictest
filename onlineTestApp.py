8ca2ff1e44109b08f62d79b8a5ccf027c1848418
ik.nitk <ik.nitk@gmail.com>
2014-06-28 23:31:36 +0530

    basic auth

--- onlineTestApp.py
+++ onlineTestApp.py
@@ -1,4 +1,7 @@
 import web
+import re
+import base64
+
 
 render = web.template.render('views/')
 db = web.database(dbn='postgres', user='dbuser1', pw='dbuser1', db='en2ardb')
@@ -7,7 +10,8 @@ urls = (
     '/', 'index',
     '/add','add',
     '/qaitems','qaitems', 
-    '/search','search' 
+    '/search','search',
+    '/login' , 'Login' 
 )
 
 class index:
@@ -29,15 +33,42 @@ class search:
 
 class qaitems:
     def GET(self):
-        todos = db.query('select * from qaitems order by id desc limit 10')
-        return render.qaitems(todos)
+        if web.ctx.env.get('HTTP_AUTHORIZATION') is not None:
+            todos = db.query('select * from qaitems order by id desc limit 10')
+            return render.qaitems(todos)
+        else:
+            raise web.seeother('/login')
 
 class add:
     def POST(self):
         i = web.input()
         n = db.insert('qaitems',question=i.question,answer=i.answer,tags=i.tags)
         raise web.seeother('/qaitems')
+
+#poor auth. but just a temp fix
+allowed = (
+    ('arabic','english'),
+    ('english','arabic')
+)
+
             
+class Login:
+    def GET(self):
+        auth = web.ctx.env.get('HTTP_AUTHORIZATION')
+        authreq = False
+        if auth is None:
+            authreq = True
+        else:
+            auth = re.sub('^Basic ','',auth)
+            username,password = base64.decodestring(auth).split(':')
+            if (username,password) in allowed:
+                raise web.seeother('/qaitems')
+            else:
+                authreq = True
+        if authreq:
+            web.header('WWW-Authenticate','Basic realm="Need login"')
+            web.ctx.status = '401 Unauthorized'
+            return
 
 
 if __name__ == "__main__":
